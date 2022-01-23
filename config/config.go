b0fb73cf739412dfbf963d83dc7e7aa4e9e6089a
TalkBeCheap <xfy150150@gmail.com>
2022-01-23 18:32:27 +0800

    Viperconfig热更新配置

---
+++ config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Name string
+}
+
+func Init(cfg string) error {
+	c := Config{
+		Name: cfg,
+	}
+
+	// 初始化配置文件
+	if err := c.initConfig(); err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			fmt.Println(viper.GetString("runmode"))
+			time.Sleep(4 * time.Second)
+		}
+	}()
+
+	// 监控配置文件变化并热加载程序
+	c.watchConfig()
+
+	return nil
+}
+
+func (c *Config) initConfig() error {
+	if c.Name != "" {
+		viper.SetConfigFile(c.Name) // 如果指定了配置文件，则解析指定的配置文件
+	} else {
+		viper.AddConfigPath("./") // 如果没有指定配置文件，则解析默认的配置文件 ./代表项目目录
+		viper.SetConfigName("config")
+	}
+	viper.SetConfigType("yaml")     // 设置配置文件格式为YAML
+	viper.AutomaticEnv()            // 读取匹配的环境变量
+	viper.SetEnvPrefix("APISERVER") // 读取环境变量的前缀为APISERVER
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	if err := viper.ReadInConfig(); err != nil { // viper解析配置文件
+		return err
+	}
+
+	return nil
+}
+
+// 监控配置文件变化并热加载程序
+func (c *Config) watchConfig() {
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.Printf("Config file changed: %s", e.Name)
+	})
+}
